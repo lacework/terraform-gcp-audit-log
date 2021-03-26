8ea363c8353256c720e8af665e4cb6487387d510
Mike Laramie <mike.laramie@lacework.net>
2021-03-26 14:17:07 -0400

    Added examples for UBLA and OLM

--- examples/project-level-audit-log/main.tf
+++ examples/project-level-audit-log/main.tf
@@ -5,4 +5,6 @@ provider "lacework" {}
 module "gcp_organization_level_audit_log" {
   source               = "../../"
   bucket_force_destroy = true
+  enable_ubla          = true
+  lifecycle_rule_age   = 7
 }
