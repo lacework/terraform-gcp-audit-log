30e1749cd7ad658e5835f33d004689b4c7b11439
Ross Moles <ross.moles@lacework.net>
2021-04-19 15:00:45 +0100

    ci: add release_contains_features() & check_for_minor_version_bump()
    Signed-off-by: Ross Moles <ross.moles@lacework.net>

--- scripts/release.sh
+++ scripts/release.sh
@@ -212,6 +212,21 @@ open_pull_request() {
   log "    $_pr_url"
 }
 
+release_contains_features() {
+  latest_version=$(find_latest_version)
+  git log --no-merges --pretty="%s" ${latest_version}..main | grep "feat[:(]" >/dev/null
+  return $?
+}
+
+ check_for_minor_version_bump() {
+  if release_contains_features; then
+    log "new feature detected, minor version bump"
+    echo $VERSION | awk -F. '{printf("%d.%d.0", $1, $2+1)}' > VERSION
+    VERSION=$(cat VERSION)
+    log "updated version to v$VERSION"
+  fi
+}
+
 tag_release() {
   local _tag="v$VERSION"
   log "creating github tag: $_tag"
