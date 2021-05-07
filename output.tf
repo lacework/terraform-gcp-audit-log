0180ae239ff5cb71c2ee5b11d39842ee33d438af
mr-menno <5025196+mr-menno@users.noreply.github.com>
2021-05-07 09:29:15 -0700

    fix: support for sensitive values (#20)
    Fix for ALLY-452
    
    Signed-off-by: menno <menno@lacework.com>

--- output.tf
+++ output.tf
@@ -6,6 +6,7 @@ output "service_account_name" {
 output "service_account_private_key" {
   value       = length(var.service_account_private_key) > 0 ? var.service_account_private_key : module.lacework_at_svc_account.private_key
   description = "The private key in JSON format, base64 encoded"
+  sensitive   = true
 }
 
 output "bucket_name" {
