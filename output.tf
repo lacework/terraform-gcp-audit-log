067c58d2329c23e991c0390af14eb459eab9db00
Michael Droessler <michael.droessler@lacework.net>
2021-10-05 09:39:17 -0500

    added output warning when using pre-existing sink & bucket

--- output.tf
+++ output.tf
@@ -18,3 +18,13 @@ output "pubsub_topic_name" {
   value       = google_pubsub_topic.lacework_topic.name
   description = "The PubSub topic name"
 }
+
+output "sink_name" {
+  value       = local.sink_name
+  description = "The sink name"
+}
+
+output "existing_sink_warning" {
+  value       = length(var.existing_sink_name) > 0 && length(var.existing_bucket_name) > 0 ? "When using a pre-existing sink and pre-existing storage bucket, customers must ensure the bucket is configured as the destination for the sink!" : ""
+  description = "Warning for customers using pre-existing sink and bucket"
+}
