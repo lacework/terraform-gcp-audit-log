3e13e6af33ab99769e388b95fa4364baed2cd353
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2022-02-10 21:42:36 +0000

    feat: allow google provider version >= 3.0.0, < 5.0.0 (#47)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.31"
 
   required_providers {
-    google = "~> 3.0"
+    google = ">= 3.0.0, < 5.0.0"
     time   = "~> 0.6"
     lacework = {
       source  = "lacework/lacework"
