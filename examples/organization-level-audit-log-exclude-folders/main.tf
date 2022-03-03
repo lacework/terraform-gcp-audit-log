6f0b61ab36aa4eec2aaa598182bb9b1ae5ae28fe
Mike Laramie <mike.laramie@lacework.net>
2022-03-03 14:08:37 -0500

    updated test case

--- examples/organization-level-audit-log-exclude-folders/main.tf
+++ examples/organization-level-audit-log-exclude-folders/main.tf
@@ -6,13 +6,12 @@ module "gcp_organization_level_audit_log" {
   source               = "../../"
   bucket_force_destroy = true
   org_integration      = true
-  organization_id      = "889612674382"
-  project_id = "lwint-mikelaramie"
+  organization_id      = "my-organization-id"
   enable_ubla          = true
   lifecycle_rule_age   = 7
   exclude_folders      = true
-  folders_to_exclude   = [
-    "folders/578370918314", 
-    "folders/1099205162015",
-  ] 
+  folders_to_exclude = [
+    "folders/123456789012",
+    "folders/345678901234",
+  ]
 }
