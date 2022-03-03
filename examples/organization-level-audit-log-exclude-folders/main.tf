7a52dc3c71e83cb028f721d8d5cc3411e82d94b8
Mike Laramie <mike.laramie@lacework.net>
2022-03-03 15:44:56 -0500

    adding org_id var

--- examples/organization-level-audit-log-exclude-folders/main.tf
+++ examples/organization-level-audit-log-exclude-folders/main.tf
@@ -6,7 +6,7 @@ module "gcp_organization_level_audit_log" {
   source               = "../../"
   bucket_force_destroy = true
   org_integration      = true
-  organization_id      = "my-organization-id"
+  organization_id      = var.organization_id
   enable_ubla          = true
   lifecycle_rule_age   = 7
   exclude_folders      = true
