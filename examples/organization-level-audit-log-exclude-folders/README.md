55e0f633a738931fa215743d3acd802ea00428fc
Mike Laramie <mike.laramie@lacework.net>
2022-03-28 14:19:59 -0400

    docs: updated links and variables in examples/ (#52)

--- examples/organization-level-audit-log-exclude-folders/README.md
+++ examples/organization-level-audit-log-exclude-folders/README.md
@@ -17,6 +17,7 @@ provider "lacework" {}
 
 module "gcp_organization_level_audit_log" {
   source               = "../../"
+  version              = "~> 3.0"
   bucket_force_destroy = true
   org_integration      = true
   organization_id      = "my-organization-id"
--- variables.tf
+++ variables.tf
@@ -129,11 +129,11 @@ variable "k8s_filter" {
 variable "folders_to_exclude" {
   type        = list(string)
   default     = []
-  description = "List of root folders to exclude if `exclude_folders` is set to `true`.  Format is 'folders/1234567890'"
+  description = "List of root folders to exclude in an organization-level integration.  Format is 'folders/1234567890'"
 }
 
 variable "include_root_projects" {
   type        = bool
   default     = true
-  description = "Enables logic to include root-level projects if `exclude_folders` is set to `true`.  Default is true"
+  description = "Enables logic to include root-level projects if excluding folders.  Default is true"
 }
