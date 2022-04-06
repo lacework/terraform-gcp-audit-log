589f6f3e2be95b75147c5c6810f18862d59926ed
Tim Arenz <tim@arenz.cc>
2022-04-06 11:05:43 +0200

    feat: Add project id variable validation (#53)

--- variables.tf
+++ variables.tf
@@ -24,6 +24,10 @@ variable "project_id" {
   type        = string
   default     = ""
   description = "A project ID different from the default defined inside the provider"
+  validation {
+    condition     = can(regex("(^[a-z][a-z0-9_-]{6,30}[^-]$|^$)", var.project_id))
+    error_message = "The project_id variable must be a valid GCP project ID. It must be 6 to 30 lowercase ASCII letters, digits, or hyphens. It must start with a letter. Trailing hyphens are prohibited.. Example: tokyo-rain-123."
+  }
 }
 
 variable "use_existing_service_account" {
