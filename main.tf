bea2846a423682a7ca99e36122e4fbbdbf65dc03
Darren Murray <darren.murray@lacework.net>
2024-01-23 17:01:11 +0000

    chore: set local var module name
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ locals {
   ]
 
   version_file   = "${abspath(path.module)}/VERSION"
-  module_name    = basename(abspath(path.module))
+  module_name    = "terraform-gcp-audit-log"
   module_version = fileexists(local.version_file) ? file(local.version_file) : ""
 }
 
